311d2fe94c6e8d19fedf46540ec3683cb60a8707
igorazuos <azuosigor@gmail.com>
2022-09-14 13:56:45 -0300

    atualização exercicico1 a017

---
+++ exercicio1/script.js
@@ -0,0 +1,28 @@
+// //=========== A ===========
+// function digaNome(nome) {
+//     console.log(`Olá, ${nome}`)
+// }
+// digaNome("igor")
+
+// //=========== B ===========
+// function tabuada6(num1, num2){
+//     for (let i = 0; i <= 10; i++) {
+//         console.log(i*6)
+        
+//     }
+// }
+// tabuada6()
+
+//=========== C.1 ===========
+const digaNome = (nome) =>{
+    console.log(`Olá, ${nome}`)
+}
+digaNome("igor")
+
+//=========== C.2 ===========
+const tabuada6 = (num1, num2) => {
+    for(let i = 0; i <= 10; i++){
+        console.log(i*6)
+    }
+}
+tabuada6()
